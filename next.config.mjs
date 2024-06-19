60e54932763747349170b545c9a391d4960fe470
NahidNoorshat <abunahid754@gmail.com>
2024-06-19 22:19:25 +0600

    no change

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
-import withCloudflarePages from "@cloudflare/next-on-pages";
 /** @type {import('next').NextConfig} */
-const nextConfig = withCloudflarePages({
-  reactStrictMode: true,
+const nextConfig = {
   images: {
     domains: ["127.0.0.1"], // Add your domain here for localhost
   },
   experimental: {
-    runtime: "edge", // Necessary for Cloudflare Workers compatibility
     missingSuspenseWithCSRBailout: false,
   },
-});
+};
 
 export default nextConfig;
