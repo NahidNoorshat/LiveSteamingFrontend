98e467754ea7a28700c1ea77c9d398b6182b7061
NahidNoorshat <abunahid754@gmail.com>
2024-06-19 22:08:16 +0600

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
+import { withCloudflarePages } from "@cloudflare/next-on-pages";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withCloudflarePages({
+  reactStrictMode: true,
   images: {
     domains: ["127.0.0.1"], // Add your domain here for localhost
   },
   experimental: {
+    runtime: "edge", // Necessary for Cloudflare Workers compatibility
     missingSuspenseWithCSRBailout: false,
   },
-};
+});
 
 export default nextConfig;
